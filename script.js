ebb63702df300ef8f31b2ba9267ca7a2a05bbb2a
Amin bouzouita <Aminbouzouita@yahoo.com>
2020-07-05 23:30:05 -0700

    emplacement again

--- script.js
+++ script.js
@@ -1,28 +1,28 @@
-// //Assignment Code
-// var generateBtn = document.querySelector("#generate");
+//Assignment Code
+var generateBtn = document.querySelector("#generate");
 
-// //Write password to the #password input
-// var password = generatePassword();
-// var passwordText = document.querySelector("#password");
-//     passwordText.value = password;
-// //Add event listener to generate button
-//  generateBtn.addEventListener("click", writePassword);
- 
+//Write password to the #password input
+
+var passwordText = document.querySelector("#password");
   
-  
-function generatePassword(){
+//Add event listener to generate button
+ generateBtn.addEventListener("click", function(event){
+event.preventDefault();
+  var lengthpassword = prompt(`How many characters do you like in your password ? minimum 8 and maximum 128 ?`);
+  var uppercasech = confirm(`Do you like uppercase characters in your password? ABCDEFG..`);
+  var lowercasech = confirm(`Do you like lowercase characters in your password? abcdefg..`);
+  var numbersech = confirm(`Do you like numbers in your password? 0123456..`);
+  var specialch = confirm(`Do you like special characters in your password? @#$%^&*()..`);
+writePassword();
+
+function writePassword(){
   const lowercase = [`a`,`b`,`c`,`d`,`e`,`f`,`g`,`h`,`i`,`j`,`k`,`l`,`m`,`n`,`o`,`p`,`q`,`r`,`s`,`t`,`u`,`v`,`w`,`x`,`y`,`z`]; 
   const uppercase = [`A`,`B`,`C`,`D`,`E`,`F`,`G`,`H`,`I`,`J`,`K`,`L`,`M`,`N`,`O`,`P`,`Q`,`R`,`S`,`T`,`U`,`V`,`W`,`X`,`Y`,`Z`];
   const spcharacter = [`!`,`@`,`#`,`$`,`%`,`^`,`&`,`*`,`/`,`"`,`(`,`)`,`-`,`_`,`=`,`+`,`|`,`[`,`]`];
   let password = ``;
   const number = [`0`,`1`,`2`,`3`,`4`,`5`,`6`,`7`,`8`,`9`];
   let i = 0;
-  let lengthpassword = prompt(`How many characters do you like in your password ? minimum 8 and maximum 128 ?`);
-  var uppercasech = confirm(`Do you like uppercase characters in your password? ABCDEFG..`);
-  var lowercasech = confirm(`Do you like lowercase characters in your password? abcdefg..`);
-  var numbersech = confirm(`Do you like numbers in your password? 0123456..`);
-  var specialch = confirm(`Do you like special characters in your password? @#$%^&*()..`);
-  while ( i < 129 ){
+  while ( i < lengthpassword ){
 
     if (uppercasech === true){ 
      
@@ -48,6 +48,11 @@ function generatePassword(){
     
   }
   console.log(password);
-return password;
+passwordText.textContent= password;
 }  
- generatePassword(); 
+ 
+
+ 
+  
+
+ });
